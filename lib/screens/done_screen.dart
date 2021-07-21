0cfa8c9bc96b4d24b1bfb04dd58f48c8b698efd6
baseplate77 <baseplate8087@gmail.com>
2021-07-21 19:54:49 +0530

    splash and done screen

--- lib/screens/done_screen.dart
+++ lib/screens/done_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tourve/core/navigation.dart';
+import 'package:tourve/screens/tabs/home_tab.dart';
+
+import '../constant.dart';
 
 class DoneScreen extends StatefulWidget {
   const DoneScreen({Key? key}) : super(key: key);
@@ -8,12 +12,32 @@ class DoneScreen extends StatefulWidget {
 
 class _DoneScreenState extends State<DoneScreen> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 3500), () {
+      // navigate(context, );
+      // Navigator.pop(context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const <Widget>[
-          Icon(Icons.done),
+        children: <Widget>[
+          Image.asset(
+            "assets/images/confirm_animation.gif",
+          ),
+          const SizedBox(
+            height: 42,
+          ),
+          Text("Your booking has been Confirm",
+              style: kQuoteStyle.copyWith(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18)),
         ],
       ),
     );
